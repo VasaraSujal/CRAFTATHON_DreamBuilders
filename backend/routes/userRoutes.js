4fd743d6303ddedf09d12e299619da55d5f716a1
Dhruvil <dhruvilpatel.m@gmail.com>
2026-04-03 23:11:01 +0530

    Let admins create users and change roles from one place

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, authUser, getAllUsers, deleteUser } = require('../controllers/userController');
+const {
+	registerUser,
+	authUser,
+	createUserByAdmin,
+	getAllUsers,
+	updateUserRole,
+	deleteUser,
+} = require('../controllers/userController');
 const { protect, authorize } = require('../middleware/auth');
 
 router.post('/', registerUser);
 router.post('/login', authUser);
 
 // Admin-only endpoints
+router.post('/admin', protect, authorize('Admin'), createUserByAdmin);
 router.get('/', protect, authorize('Admin'), getAllUsers);
+router.patch('/:id/role', protect, authorize('Admin'), updateUserRole);
 router.delete('/:id', protect, authorize('Admin'), deleteUser);
 
 module.exports = router;
